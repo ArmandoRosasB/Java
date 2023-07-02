4b0813741123e018786b3f64455a950035091f26
armando <armando.rosas133@gmail.com>
2023-07-01 23:28:39 -0600

    operadores

---
+++ Operadores/OperadoresAsignacion.java
@@ -0,0 +1,29 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = i + 4;
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        i += 2; // i = i + 2
+
+        System.out.println("i += 2  -> " + i);
+
+        i += 5; // i = i + 5
+        System.out.println("i += 5 -> " + i);
+
+        j -= 4; // j = j - 4
+        System.out.println("j -= 4 -> " + j);
+
+        j *= 3;
+        System.out.println("j *= 3 -> " + j);
+
+        String sqlString = "SELECT * FROM clientes AS c";
+        sqlString += " WHERE c.nombre = 'Andres'";
+        sqlString += " AND c.activo = 1";
+
+        System.out.println("sqlString = " + sqlString);
+    }
+    
+}
