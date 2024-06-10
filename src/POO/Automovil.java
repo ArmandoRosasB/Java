79dcdc899c92bdb5798a7daccd2dd83589153943
ArmandoRosasB <you@example.com>
2024-06-09 22:35:51 -0600

    Fin de la introduccion a POO

--- src/POO/Automovil.java
+++ src/POO/Automovil.java
@@ -1,7 +1,8 @@
 package POO;
 
-public class Automovil {
+public class Automovil implements Comparable<Automovil> {
         private final int id;
+        private int indice_ruedas = 0;
 
         private String fabricante;
         private String modelo;
@@ -17,6 +18,13 @@ public class Automovil {
         public static final int VELOCIDAD_MAXIMA_CARRETERA = 130;
         public static final  int VELOCIDAD_MAXIMA_CIUDAD = 80;
 
+        public Automovil () {
+                this.id = ++total_autos;
+                this.fabricante = "Toyota";
+                this.modelo = "Corola";
+                this.color = Color.BLANCO;
+                this.tipo = TipoAutomovil.FAMILIAR;
+        }
         public Automovil(String fabricante, String modelo, Color color, TipoAutomovil tipo){
                 this.id = ++total_autos;
                 this.fabricante = fabricante;
@@ -28,6 +36,13 @@ public class Automovil {
         public Automovil(String fabricante, String modelo, Color color, TipoAutomovil tipo, Motor motor){
                 this(fabricante, modelo, color, tipo);
                 this.motor = motor;
+        }
+
+        public Automovil(String fabricante, String modelo, Color color, TipoAutomovil tipo, Motor motor, Tanque tanque, Persona conductor, Rueda[] ruedas){
+                this(fabricante, modelo, color, tipo, motor);
+                this.tanque = tanque;
+                this.conductor = conductor;
+                this.ruedas = ruedas;
         }
 
         public String getFabricante() {
@@ -50,6 +65,10 @@ public class Automovil {
                 return this.tipo;
         }
 
+        public Persona getConductor() { return this.conductor; }
+
+        public Rueda [] getRuedas() { return this.ruedas; }
+
         public void setFabricante(String fabricante) {
                 this.fabricante = fabricante;
         }
@@ -68,6 +87,21 @@ public class Automovil {
 
         public void setTipoAutomovil(TipoAutomovil tipo){
                 this.tipo = tipo;
+        }
+
+        public void setConductor (Persona conductor) {this.conductor = conductor; }
+
+        public void setRuedas (Rueda [] ruedas) { this.ruedas = ruedas; }
+
+        public Automovil addRueda (Rueda rueda){
+                if (ruedas == null){
+                        ruedas = new Rueda[5];
+                }
+                if (indice_ruedas < ruedas.length) {
+                        ruedas[indice_ruedas++] = rueda;
+                }
+
+                return this;
         }
 
         public String acelerar(int rpm){
@@ -89,8 +123,27 @@ public class Automovil {
                 sb.append("Color - ").append(this.color).append('\n');
                 sb.append("Tipo - ").append(this.tipo.getNombre()).append(" de ").append(this.tipo.getNumeroPuertas()).append(" puertas").append('\n');
 
-                if (motor != null) sb.append("Cilindrada - ").append(this.motor.getCilindrada()).append('\n');
+                if (motor != null) {
+                        sb.append("Motor - ").append("\n\t").append("Cilindrada - ").append(this.motor.getCilindrada()).append("\n\t").append("Tipo - ").append(this.motor.getTipo()).append('\n');
+                }
 
+                if (tanque != null){
+                        sb.append("Capacidad del tanque - ").append(tanque.getCapacidad()).append('\n');
+                }
+
+                if (conductor != null) {
+                        sb.append("Conductor - ").append(this.conductor).append('\n');
+                }
+
+                if (ruedas != null) {
+                        for(int i = 0; i < ruedas.length; i++){
+                                sb.append("Rueda ").append(i + 1).append("- ").append("\n\t")
+                                        .append("Fabricante - ").append(ruedas[i].getFabricante()).append("\n\t")
+                                        .append("Ancho - ").append(ruedas[i].getAncho()).append("\n\t")
+                                        .append("Aro - ").append(ruedas[i].getAro())
+                                        .append('\n');
+                        }
+                }
                 return sb.toString();
         }
 
@@ -99,6 +152,11 @@ public class Automovil {
                 if (this == other) return true;
                 if (!(other instanceof Automovil automovil)) return false;
                 return (this.fabricante.equals(automovil.fabricante) && this.modelo.equals(automovil.modelo));
+        }
+
+        @Override
+        public int compareTo(Automovil other){
+                return this.fabricante.compareTo(other.getFabricante());
         }
 
 }
--- src/POO/Main.java
+++ src/POO/Main.java
@@ -1,10 +1,35 @@
 package POO;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Automovil auto_uno = new Automovil("Ford", "Mustang", Color.ROJO, TipoAutomovil.DEPORTIVO);
         Automovil auto_dos = new Automovil("Chevrolet", "Camaro", Color.NEGRO, TipoAutomovil.DEPORTIVO);
-        Automovil auto_tres = new Automovil("Ford", "Shelby", Color.BLANCO, TipoAutomovil.DEPORTIVO, new Motor(5.0, TipoMotor.BENCINA));
+        Automovil auto_tres = new Automovil("Ford", "Mustang Shelby GT500", Color.BLANCO, TipoAutomovil.DEPORTIVO, new Motor(5.0, TipoMotor.BENCINA), new Tanque(50), new Persona("Armando", "Rosas"), null);
+
+        Persona conductor_uno = new Persona("Ivan", "Rosas");
+        Persona conductor_dos = new Persona("Laura", "Rosas");
+
+        Rueda [] ruedas = new Rueda[4];
+        for(int i = 0; i < ruedas.length; i++){
+            ruedas[i] = new Rueda("Michelin", 18, 10.5);
+        }
+
+        auto_uno.setConductor(conductor_uno);
+        auto_dos.setConductor(conductor_dos);
+
+        auto_uno.setRuedas(ruedas);
+        auto_dos.addRueda(new Rueda("Michelin", 18, 10.5))
+                .addRueda(new Rueda("Michelin", 18, 10.5))
+                .addRueda(new Rueda("Michelin", 18, 10.5))
+                .addRueda(new Rueda("Michelin", 18, 10.5))
+                .addRueda(new Rueda("Michelin", 18, 10.5));
+
+        for(int i = 0; i < 5; i++){
+            auto_tres.addRueda(new Rueda("Good year", 19, 11));
+        }
+
 
         auto_uno.setFabricante("Ford");
         auto_uno.setModelo("Mustang");
@@ -30,5 +55,18 @@ public class Main {
         System.out.println("Total autos: " + Automovil.total_autos);
         System.out.println("Velocidad maxima en carretera: " + Automovil.VELOCIDAD_MAXIMA_CARRETERA);
         System.out.println("Velocidad maxima en ciudad: " + Automovil.VELOCIDAD_MAXIMA_CIUDAD);
+
+
+        Automovil [] automoviles = {
+                auto_uno,
+                auto_dos,
+                auto_tres
+        };
+
+        Arrays.sort(automoviles);
+        for(int i = 0; i < automoviles.length; i++){
+            System.out.println(i + "- " + automoviles[i].getFabricante() + " " + automoviles[i].getModelo());
+        }
+
     }
 }
